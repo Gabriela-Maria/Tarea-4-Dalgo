f374b8e00c928d7f4b67b9bbbda9abd1f250a1a5
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-04-11 14:15:25 -0500

    bellamn ford optimizado

--- Problema1/BellmanFord.java
+++ Problema1/BellmanFord.java
@@ -10,31 +10,31 @@ public class BellmanFord {
             distancia[i] = Integer.MAX_VALUE;
         distancia[verticeInicio] = 0;
 
+        boolean huboCambios;
         for (int i = 1; i < size; ++i) {
+            huboCambios = false;
             for (int u = 0; u < size; ++u) {
                 for (int v = 0; v < size; ++v) {
                     if (grafo[u][v] != -1 && distancia[u] != Integer.MAX_VALUE && distancia[u] + grafo[u][v] < distancia[v]) {
                         distancia[v] = distancia[u] + grafo[u][v];
+                        huboCambios = true;
                     }
                 }
             }
+            if (!huboCambios) 
+                break;
         }
         return distancia;
-
-
     }
 
-    public static int[][] distanciasMinimas(int[][]grafo){
-
+    public static int[][] distanciasMinimas(int[][] grafo) {
         int size = grafo.length;
         int[][] respuesta = new int[size][size];
-        int[] listaPorVertice = new int[size];
-        for(int i =0;i<size;i++){
-            listaPorVertice = bellmanFord(grafo, i);
-            for(int j =0;j<size;j++){
-                respuesta[i][j] = listaPorVertice[j];
-            }
+
+        for (int i = 0; i < size; i++) {
+            respuesta[i] = bellmanFord(grafo, i);
         }
+
         return respuesta;
     }
 }
